d747cad637e3e605f8a6f2fdb2d697dd15b7e9ce
Ahmadsaidnouh <77753719+Ahmadsaidnouh@users.noreply.github.com>
2021-04-21 19:37:12 +0300

    added test method

--- Player.java
+++ Player.java
@@ -2,5 +2,26 @@ package com.game;
 
 public class Player
 {
-  //code here
+   public boolean test(char [][] arr , int row , int column, char ch)
+   {
+        int c,r;
+        r = row;
+        c = column;
+        //all possible conditions for a point to be true. They are 12 conditions.
+        if( (arr[r-2][c] == ch && arr[r-1][c] == ch) ||
+            (arr[r+1][c] == ch && arr[r+2][c] == ch) ||
+            (arr[r][c-2] == ch && arr[r][c-1] == ch) ||
+            (arr[r][c+1] == ch && arr[r][c+2] == ch) ||
+            (arr[r-1][c+1] == ch && arr[r-2][c+2] == ch) ||
+            (arr[r+1][c+1] == ch && arr[r+2][c+2] == ch) ||
+            (arr[r-2][c-2] == ch && arr[r-1][c-1] == ch) ||
+            (arr[r+1][c-1] == ch && arr[r+2][c-2] == ch) ||
+            (arr[r][c-1] == ch && arr[r][c+1] == ch) ||
+            (arr[r-1][c] == ch && arr[r+1][c] == ch) ||
+            (arr[r-1][c+1] == ch && arr[r+1][c-1] == ch) ||
+            (arr[r-1][c-1] == ch && arr[r+1][c+1] == ch)  )
+            return true;
+        else
+            return false;
+   }
 }
