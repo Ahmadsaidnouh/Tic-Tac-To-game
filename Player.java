74e61c5e26f3f0e21b7099e98472562d82482a62
Ahmadsaidnouh <77753719+Ahmadsaidnouh@users.noreply.github.com>
2021-04-22 01:25:47 +0300

    Added some needed methods to Player class

--- Player.java
+++ Player.java
@@ -2,6 +2,29 @@ package com.game;
 
 public class Player
 {
+   private String playerName = "";
+   private char playerX_O;
+
+   public Player(String playerName, char playerX_O)
+    {
+        this.playerName = playerName;
+        this.playerX_O = playerX_O;
+    }
+   public String getPlayerName()
+    {
+        //code
+    }
+   public char getPlayerX_O()
+    {
+        //code
+    }
+   public void displayWinMessage()
+    { 
+//         System.out.println();
+//         System.out.println(".......................................");
+//         System.out.println(":Congratulations " + playerName + " won the game:");
+//         System.out.println(".......................................");
+    }
    public boolean test(char [][] arr , int row , int column, char ch)
    {
         int c,r;
