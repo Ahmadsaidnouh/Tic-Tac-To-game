e835ff7aacc15d8cdad96553ce58c5da18b42b03
Ahmadsaidnouh <77753719+Ahmadsaidnouh@users.noreply.github.com>
2021-04-23 02:42:48 +0300

    SemiFinal Player class

--- Player.java
+++ Player.java
@@ -1,50 +1,52 @@
 package com.game;
 
-public class Player
-{
-   private String playerName = "";
-   private char playerX_O;
+public class Player {
+    private String playerName = "";
+    private char playerX_O;
 
-   public Player(String playerName, char playerX_O)
+    public Player(String playerName, char playerX_O)
     {
         this.playerName = playerName;
         this.playerX_O = playerX_O;
     }
-   public String getPlayerName()
+    public String getPlayerName()
     {
-        //code
+        return playerName;
     }
-   public char getPlayerX_O()
+    public char getPlayerX_O()
     {
-        //code
+        return playerX_O;
     }
-   public void displayWinMessage()
-    { 
-//         System.out.println();
-//         System.out.println(".......................................");
-//         System.out.println(":Congratulations " + playerName + " won the game:");
-//         System.out.println(".......................................");
+    public void displayWinMessage()
+    {
+        System.out.println();
+        System.out.println("..........................................");
+        System.out.println(":Congratulations " + playerName + ". You won the game:");
+        System.out.println("..........................................");
     }
-   public boolean test(char [][] arr , int row , int column, char ch)
-   {
+    public boolean test(char [][] arr , int row , int column, char ch)
+    {
         int c,r;
         r = row;
         c = column;
-        //all possible conditions for a point to be true. They are 12 conditions.
         if( (arr[r-2][c] == ch && arr[r-1][c] == ch) ||
-            (arr[r+1][c] == ch && arr[r+2][c] == ch) ||
-            (arr[r][c-2] == ch && arr[r][c-1] == ch) ||
-            (arr[r][c+1] == ch && arr[r][c+2] == ch) ||
-            (arr[r-1][c+1] == ch && arr[r-2][c+2] == ch) ||
-            (arr[r+1][c+1] == ch && arr[r+2][c+2] == ch) ||
-            (arr[r-2][c-2] == ch && arr[r-1][c-1] == ch) ||
-            (arr[r+1][c-1] == ch && arr[r+2][c-2] == ch) ||
-            (arr[r][c-1] == ch && arr[r][c+1] == ch) ||
-            (arr[r-1][c] == ch && arr[r+1][c] == ch) ||
-            (arr[r-1][c+1] == ch && arr[r+1][c-1] == ch) ||
-            (arr[r-1][c-1] == ch && arr[r+1][c+1] == ch)  )
+                (arr[r+1][c] == ch && arr[r+2][c] == ch) ||
+                (arr[r][c-2] == ch && arr[r][c-1] == ch) ||
+                (arr[r][c+1] == ch && arr[r][c+2] == ch) ||
+                (arr[r-1][c+1] == ch && arr[r-2][c+2] == ch) ||
+                (arr[r+1][c+1] == ch && arr[r+2][c+2] == ch) ||
+                (arr[r-2][c-2] == ch && arr[r-1][c-1] == ch) ||
+                (arr[r+1][c-1] == ch && arr[r+2][c-2] == ch) ||
+                (arr[r][c-1] == ch && arr[r][c+1] == ch) ||
+                (arr[r-1][c] == ch && arr[r+1][c] == ch) ||
+                (arr[r-1][c+1] == ch && arr[r+1][c-1] == ch) ||
+                (arr[r-1][c-1] == ch && arr[r+1][c+1] == ch)  )
+        {
+            Helping.displayArray(arr);
+            this.displayWinMessage();
             return true;
+        }
         else
             return false;
-   }
+    }
 }
