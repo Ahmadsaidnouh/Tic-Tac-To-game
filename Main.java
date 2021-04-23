c9f3e769639a7d3d77cdd6e27c37c0ea9fcd1e84
Ahmadsaidnouh <77753719+Ahmadsaidnouh@users.noreply.github.com>
2021-04-23 04:21:20 +0300

    SemiFinal Main class

--- Main.java
+++ Main.java
@@ -2,12 +2,11 @@ package com.game;
 
 import java.util.Scanner;
 
-public class Main {
-
-
-    public static void main(String[] args) {
+public class Main 
+{
         char plX_O;
         String plName;
+        String testingXOrOLength;
         Scanner playerNameSc = new Scanner(System.in);
         Scanner playerX_OSc = new Scanner(System.in);
         //scan the information of player 1.
@@ -16,7 +15,11 @@ public class Main {
         do
         {
             System.out.print("Player 1 choose 'x' or 'o': ");
-            plX_O = playerX_OSc.next().charAt(0);
+            testingXOrOLength = playerX_OSc.next();
+            if(testingXOrOLength.length() != 1)
+                plX_O = 'z';
+            else
+                plX_O = testingXOrOLength.charAt(0);
         }while( !Helping.isXOrO(plX_O) );
         //constructing player1 object and setting the inputs to it.
         Player player1 = new Player(plName, plX_O);
@@ -37,18 +40,19 @@ public class Main {
         //constructing player2 object and setting the inputs to it.
         Player player2 = new Player(plName, plX_O);
         System.out.println();
-        System.out.println(".......THE GAME STAAAARTS........");
+        System.out.println("........THE GAME STAAAARTS........");
         Scanner rowSc = new Scanner(System.in);
         Scanner columnSc = new Scanner(System.in);
         boolean isWinnerFound = false;
         int row,column,revolver=1;
-        int i,j;     //for iterating in for loops
+        int i,j;        //for iterating in for loops
         char[][] arr = new char[10][11];
         //setting the default character in the 2D array to be a space character ' '.
         for( i = 0; i < 9; i++)
             for( j = 0; j < 10; j++)
                 arr[i][j] = ' ';
-            
+
+        String testingStringLength;
         while( (!isWinnerFound) && (revolver <= 42) )
         {
             Helping.displayArray(arr);
@@ -62,14 +66,22 @@ public class Main {
                     //entering and validating that row is in range{1--->6}
                     do
                     {
-                        System.out.print("Enter " + player1.getPlayerX_O() + " in the row: ");
-                        row = rowSc.nextInt() + 1;
+                        System.out.print("Enter '" + player1.getPlayerX_O() + "' in the row: ");
+                        testingStringLength = rowSc.next();
+                        if( testingStringLength.length() != 1)
+                            row = 100;
+                        else
+                            row = testingStringLength.charAt(0) - 48 + 1;
                     }while( !Helping.isValidRow(row) );
                     //entering and validating column is in range{1--->7}
                     do
                     {
-                        System.out.print("Enter " + player1.getPlayerX_O() + " in the column: ");
-                        column = columnSc.nextInt() + 1;
+                        System.out.print("Enter '" + player1.getPlayerX_O() + "' in the column: ");
+                        testingStringLength = columnSc.next();
+                        if( testingStringLength.length() != 1)
+                            column = 100;
+                        else
+                            column = testingStringLength.charAt(0) - 48 + 1;
                     }while( !Helping.isValidColumn(column) );
                 }while(!Helping.isEmptyPlace(arr[row][column], row, column));
                 arr[row][column] = player1.getPlayerX_O();
@@ -85,14 +97,22 @@ public class Main {
                     //entering and validating row is in range{1--->6}
                     do
                     {
-                        System.out.print("Enter " + player2.getPlayerX_O() + " in the row: ");
-                        row = rowSc.nextInt() + 1;
+                        System.out.print("Enter '" + player2.getPlayerX_O() + "' in the row: ");
+                        testingStringLength = rowSc.next();
+                        if( testingStringLength.length() != 1)
+                            row = 100;
+                        else
+                            row = testingStringLength.charAt(0) - 48 + 1;
                     }while( !Helping.isValidRow(row) );
                     //entering and validating column is in range{1--->7}
                     do
                     {
-                        System.out.print("Enter " + player2.getPlayerX_O() + " in the column: ");
-                        column = columnSc.nextInt() + 1;
+                        System.out.print("Enter '" + player2.getPlayerX_O() + "' in the column: ");
+                        testingStringLength = columnSc.next();
+                        if( testingStringLength.length() != 1)
+                            column = 100;
+                        else
+                            column = testingStringLength.charAt(0) - 48 + 1;
                     }while( !Helping.isValidColumn(column) );
                 }while(!Helping.isEmptyPlace(arr[row][column], row, column));
                 arr[row][column] = player2.getPlayerX_O();
